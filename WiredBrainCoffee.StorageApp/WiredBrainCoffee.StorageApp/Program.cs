d9433739213e4ce8e8cad808c972b641d8789400
yendepgai <hoangyen090794@gmail.com>
2022-08-13 22:41:48 +0700

    Generics implement

---
+++ WiredBrainCoffee.StorageApp/WiredBrainCoffee.StorageApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using WiredBrainCoffee.StorageApp.Entities;
+using WiredBrainCoffee.StorageApp.Responsitories;
+
+namespace WiredBrainCoffee.StorageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employeeRepository = new EmployeeRepository();
+            employeeRepository.Add(new Employee { FirstName = "Susan" });
+            employeeRepository.Add(new Employee { FirstName = "Phong" });
+            employeeRepository.Add(new Employee { FirstName = "Yen" });
+            employeeRepository.Save();
+
+            Console.ReadLine();
+        }
+    }
+}
